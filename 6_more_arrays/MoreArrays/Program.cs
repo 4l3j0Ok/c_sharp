bcf90f5f27567e259f38b6167af156e10d7fa631
Alejo <alejofsarmiento@gmail.com>
2023-05-12 22:01:00 -0300

    new exercises

--- 6_more_arrays/MoreArrays/Program.cs
+++ 6_more_arrays/MoreArrays/Program.cs
@@ -6,16 +6,33 @@ namespace more_arrays
     {
         public static void Main(string[] args)
         {
-            ejercicio_uno();
+            //ejercicio_uno();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //ejercicio_dos();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //ejercicio_tres();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //ejercicio_cuatro();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //ejercicio_cinco();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //ejercicio_seis();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            ejercicio_siete();
             Console.WriteLine();
             Console.ReadKey();
-            ejercicio_dos();
-            Console.WriteLine();
-            Console.ReadKey();
-            ejercicio_tres();
-            Console.WriteLine();
-            Console.ReadKey();
-            ejercicio_cuatro();
         }
 
         public static void ejercicio_uno()
@@ -91,6 +108,60 @@ namespace more_arrays
                     else if ((row == total_rows -1) && (column == 0 || column == total_columns - 1)) values[row, column] = "1 ";
                     else values[row, column] = "0 ";
                     Console.Write(values[row, column]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ejercicio_cinco()
+        {
+            int total_columns = 6;
+            int total_rows = 4;
+            int value = 0;
+            int[,] values = new int[total_rows, total_columns];
+
+            for (int row = 0; row != total_rows; row += 1)
+            {
+                for (int column = 0; column != total_columns; column += 1)
+                {
+                    values[row, column] = value;
+                    Console.Write(values[row, column] + " ");
+                    if (column == total_columns -1) value = 0;
+                    else value += 2;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ejercicio_seis()
+        {
+            int total_columns = 5;
+            int total_rows = 5;
+            int[,] values = new int[total_rows, total_columns];
+
+            for (int row = 0; row != total_rows; row += 1)
+            {
+                for (int column = 0; column != total_columns; column += 1)
+                {
+                    if (column == 0) values[row, column] = 0;
+                    else values[row, column] = row + values[row, column -1];
+                    Console.Write(values[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void ejercicio_siete()
+        {
+            int total_columns = 4;
+            int total_rows = 4;
+            int[,] values = new int[total_rows, total_columns];
+
+            for (int row = 0; row != total_rows; row += 1)
+            {
+                for (int column = 0; column != total_columns; column += 1)
+                {
+                    values[row, column] = row * row;
+                    Console.Write(values[row, column] + " ");
                 }
                 Console.WriteLine();
             }
