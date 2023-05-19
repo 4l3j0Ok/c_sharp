db31f37e574183b44d06f7e241cc77fad1c481fc
Alejo <alejofsarmiento@gmail.com>
2023-05-18 22:27:07 -0300

    Added second exercise

--- 7_while_loops/Program.cs
+++ 7_while_loops/Program.cs
@@ -3,11 +3,12 @@
 namespace while_loops {
     class Program {
         static void Main(){
-            ejercicio_uno();
+            //ejercicio_uno();
+            ejercicio_dos();
         }
 
         static void ejercicio_uno() {
-            int array_length = 10;
+            const int array_length = 10;
             int index = 0;
             string result = "";
             int[] values = new int [array_length];
@@ -23,6 +24,29 @@ namespace while_loops {
             }
             Console.WriteLine("El contenido del arreglo resultante es el siguiente:");
             Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        static void ejercicio_dos()
+        {
+            int total_columns = 2;
+            int total_rows = 5;
+            int counter = 0;
+            string[,] values = new string[total_rows, total_columns];
+            
+            while(counter != total_rows) {
+                Console.Write("Ingrese el nombre: ");
+                values[counter, 1] = Console.ReadLine();
+                Console.Write("Ingrese el apellido: ");
+                values[counter, 0] = Console.ReadLine();
+                counter += 1;
+            }
+            Console.Clear();
+            Console.WriteLine("Nombres ingresados:\n");
+            for (int row = 0; row != total_rows; row += 1)
+            {
+                Console.Write("{0}. {1} {2}.\n",(row + 1), values[row, 1], values[row, 0]);
+            }
+            Console.ReadKey();
         }
     }
 }
