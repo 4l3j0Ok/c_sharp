fa9ae817ad7dd7f188f3743427239ce5213ab926
Alejo <alejofsarmiento@gmail.com>
2023-06-09 17:58:25 -0300

    Applying functional programming

--- 9_random/Program.cs
+++ 9_random/Program.cs
@@ -10,31 +10,43 @@ namespace random {
         static void first_exercise()
         {
             int arr_rows = 6, arr_columns = 4;
-            string[,] cardboard = new string[arr_rows, arr_columns];
-            int max_attempts = 3;
-            int raffled, cardboard_value;
-            string str_raffled;
-            string[] all_values = new string[arr_rows * arr_columns];
-            
+            string[,] cardboard = get_and_print_cardboard(arr_rows, arr_columns);
+            start_game(cardboard, arr_rows, arr_columns);
+        }
 
+        private static string[,] get_and_print_cardboard(int arr_rows, int arr_columns) {
+            string[,] cardboard = new string[arr_rows, arr_columns];
+            int cardboard_value;
+            string[] all_values = new string[cardboard.Length];
             // Completing array.
             Console.WriteLine("Tu cartón es el siguiente: ");
             for (int row = 0; row != arr_rows; row += 1)
             {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
                 for (int column = 0; column != arr_columns; column += 1)
                 {
-                    while (true) {
+                    Console.Write("|");
+                    while (true)
+                    {
                         cardboard_value = new Random().Next(0, 100);
                         if (!all_values.Contains(cardboard_value.ToString())) break;
                     }
                     if (cardboard_value >= 10) cardboard[row, column] = cardboard_value.ToString();
                     else cardboard[row, column] = "0" + cardboard_value.ToString();
                     all_values[row * column] = cardboard_value.ToString();
-                    Console.Write(" " + cardboard[row, column]);
+                    Console.Write(" " + cardboard[row, column] + " ");
                 }
+                Console.Write("|");
                 Console.WriteLine();
             }
+            Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
+            return cardboard;
+        }
 
+        private static void start_game(string[,] cardboard, int arr_rows, int arr_columns) {
+            int max_attempts = 3;
+            int raffled;
+            string str_raffled;
             Console.WriteLine("Presiona una tecla para sortear un número: ");
             Console.ReadKey();
             for (int attempt = max_attempts; attempt > 0; attempt -= 1)
