6469fef6ca4b33a98a9ec82e73ce5627197bac69
Alejo <alejofsarmiento@gmail.com>
2023-06-09 23:54:04 -0300

    repetition fix and sort cardboard

--- first_year/9_random/Program.cs
+++ first_year/9_random/Program.cs
@@ -10,43 +10,16 @@ namespace random {
         static void first_exercise()
         {
             int arr_rows = 6, arr_columns = 4;
-            string[,] cardboard = get_and_print_cardboard(arr_rows, arr_columns);
+            
+            int[,] cardboard = create_cardboard(arr_rows, arr_columns);
             start_game(cardboard, arr_rows, arr_columns);
         }
 
-        private static string[,] get_and_print_cardboard(int arr_rows, int arr_columns) {
-            string[,] cardboard = new string[arr_rows, arr_columns];
-            int cardboard_value;
-            string[] all_values = new string[cardboard.Length];
-            // Completing array.
-            Console.WriteLine("Tu cartón es el siguiente: ");
-            for (int row = 0; row != arr_rows; row += 1)
-            {
-                Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
-                for (int column = 0; column != arr_columns; column += 1)
-                {
-                    Console.Write("|");
-                    while (true)
-                    {
-                        cardboard_value = new Random().Next(0, 100);
-                        if (!all_values.Contains(cardboard_value.ToString())) break;
-                    }
-                    if (cardboard_value >= 10) cardboard[row, column] = cardboard_value.ToString();
-                    else cardboard[row, column] = "0" + cardboard_value.ToString();
-                    all_values[row * column] = cardboard_value.ToString();
-                    Console.Write(" " + cardboard[row, column] + " ");
-                }
-                Console.Write("|");
-                Console.WriteLine();
-            }
-            Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
-            return cardboard;
-        }
-
-        private static void start_game(string[,] cardboard, int arr_rows, int arr_columns) {
+        private static void start_game(int[,] cardboard, int arr_rows, int arr_columns) {
             int max_attempts = 3;
             int raffled;
             string str_raffled;
+            print_cardboard(cardboard, arr_rows, arr_columns);
             Console.WriteLine("Presiona una tecla para sortear un número: ");
             Console.ReadKey();
             for (int attempt = max_attempts; attempt > 0; attempt -= 1)
@@ -59,7 +32,7 @@ namespace random {
                 {
                     for (int column = 0; column != arr_columns; column += 1)
                     {
-                        if (str_raffled == cardboard[row, column])
+                        if (raffled == cardboard[row, column])
                         {
                             Console.WriteLine("El número {0} está en su cartón.", str_raffled);
                             Console.WriteLine("¡¡¡¡¡¡ACABAS DE GANAR!!!!!!");
@@ -76,6 +49,71 @@ namespace random {
                 }
             }
             Console.WriteLine("Se te acabaron los intentos ):\nMás suerte la próxima.");
+        }
+
+        private static void print_cardboard(int[,] cardboard, int arr_rows, int arr_columns)
+        {
+            Console.WriteLine("Tu cartón es el siguiente: ");
+            for (int row = 0; row != arr_rows; row += 1)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
+                for (int column = 0; column != arr_columns; column += 1)
+                {
+                    Console.Write("|");
+                    if (cardboard[row, column] > 10) Console.Write(" " + cardboard[row, column] + " ");
+                    else Console.Write(" 0" + cardboard[row, column] + " ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine(string.Concat(Enumerable.Repeat("- ", (arr_columns * 3) - 1)));
+        }
+
+        private static int[,] create_cardboard(int arr_rows, int arr_columns) {
+            int[,] cardboard = new int[arr_rows, arr_columns];
+            int cardboard_value;
+            int[] all_values = new int[cardboard.Length];
+            int index = 0;
+            // Completing array.
+            for (int row = 0; row != arr_rows; row += 1)
+            {
+                for (int column = 0; column != arr_columns; column += 1)
+                {
+                    while (true)
+                    {
+                        cardboard_value = new Random().Next(0, 100);
+                        if (!all_values.Contains(cardboard_value)) break;
+                    }
+                    cardboard[row, column] = cardboard_value;
+                    all_values[index] = cardboard_value;
+                    index += 1;
+                }
+            }
+            return order_cardboard(cardboard, arr_rows, arr_columns);
+        }
+        private static int[,] order_cardboard(int[,] cardboard, int arr_rows, int arr_columns)
+        {
+            int index = 0;
+            int[] plain_array = new int[cardboard.Length];
+            for (int row = 0; row != arr_rows; row += 1)
+            {
+                for (int column = 0; column != arr_columns; column += 1)
+                {
+                    plain_array[index] = cardboard[row, column];
+                    index += 1;
+                }
+            }
+            Array.Sort(plain_array);
+            index = 0;
+            for (int row = 0; row != arr_rows; row += 1)
+            {
+                for (int column = 0; column != arr_columns; column += 1)
+                {
+                    cardboard[row, column] = plain_array[index];
+                    index += 1;
+                }
+            }
+            return cardboard;
         }
     }
 }
