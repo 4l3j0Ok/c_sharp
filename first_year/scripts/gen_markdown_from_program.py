f00a12b2b778ef6518db69f9c9f7ed0b51ab3bc1
4l3j0Ok <alejofsarmiento@gmail.com>
2023-08-22 00:54:22 -0300

    script updated

--- first_year/scripts/gen_markdown_from_program.py
+++ first_year/scripts/gen_markdown_from_program.py
@@ -1,40 +1,51 @@
 import os
 import subprocess
 
+
 FOLDERS_PATH = os.path.dirname(os.path.abspath(__file__)).split("scripts")[0]
 TEMPLATE_PATH = FOLDERS_PATH + os.sep + "scripts" + os.sep + "template.md"
 TEMPLATE = open(TEMPLATE_PATH, "r").read()
 FOLDER_NAME = "12_practical"
 COMPLETE_PATH = FOLDERS_PATH + os.sep + FOLDER_NAME
 MD_PATH = COMPLETE_PATH + os.sep + FOLDER_NAME + ".md"
+PDF_PATH = COMPLETE_PATH + os.sep + FOLDER_NAME + ".pdf"
+
 BODY = "{code}\n{output}"
 
 
-with open(MD_PATH, "w+", encoding="utf-8") as md_file:
-    print("Recorriendo los archivos .cs...")
-    for folder in os.listdir(COMPLETE_PATH):
-        if folder[0] != "0":
-            continue
-        folder_path = COMPLETE_PATH + os.sep + folder
-        print(folder)
-        for file in os.listdir(folder_path):
-            file_path = folder_path + os.sep + file
-            if not os.path.isfile(file_path):
+def create_md():
+    with open(MD_PATH, "w+", encoding="utf-8") as md_file:
+        print("Recorriendo los archivos .cs...")
+        for folder in os.listdir(COMPLETE_PATH):
+            if folder[0] != "0":
                 continue
-            if file.split(".")[1] != "cs":
-                continue
-            print(f"USANDO EL ARCHIVO: {file}")
-            print("OBTENIENDO EL CONTENIDO DEL ARCHIVO...")
-            cs_content = open(file_path, encoding="utf-8").read()
-            code = f"Código:\n```csharp\n{cs_content}\n```\n"
-            command = f"dotnet run {file_path} --project {folder_path}"
-            print(f"EJECUTANDO EL COMANDO: {command}")
-            output = "Salida:\n```\n" + subprocess.run(command, shell=True, stdin=subprocess.DEVNULL, capture_output=True, text=True, encoding="utf-8").stdout + "```"
-            title = "Ejercicio " + folder if folder[0] != "0" else "Ejercicio " + folder[1:]
-            body = BODY.format(code=code, output=output)
-            print("REEMPLAZANDO EL CONTENIDO DEL TEMPLATE...")
-            content = TEMPLATE.format(MD_TITLE=title, BODY=body)
-            print("AGREGANDO EL CONTENIDO AL ARCHIVO FINAL...")
-            md_file.write(content)
+            folder_path = COMPLETE_PATH + os.sep + folder
+            for file in os.listdir(folder_path):
+                file_path = folder_path + os.sep + file
+                if not os.path.isfile(file_path):
+                    continue
+                if file.split(".")[1] != "cs":
+                    continue
+                print(f"Usando el archivo: {file}")
+                print("Obteniendo el contenido del archivo...")
+                cs_content = open(file_path, encoding="utf-8").read()
+                code = f"Código:\n```csharp\n{cs_content}\n```\n"
+                command = f"dotnet run {file_path} --project {folder_path}"
+                print(f"Ejecutando el comando: {command}")
+                output = "Salida:\n```\n" + subprocess.run(command, shell=True, stdin=subprocess.DEVNULL, capture_output=True, text=True, encoding="utf-8").stdout + "```"
+                title = "Ejercicio " + folder if folder[0] != "0" else "Ejercicio " + folder[1:]
+                body = BODY.format(
+                    code=code,
+                    output=output
+                )
+                print("Reemplazando el contenido del template...")
+                content = TEMPLATE.format(
+                    MD_TITLE=title,
+                    BODY=body
+                )
+                print("Agregando el contenido al archivo final...")
+                md_file.write(content)
 
-print("PROCESO TERMINADO")
+
+if __name__ == "__main__":
+    create_md()
